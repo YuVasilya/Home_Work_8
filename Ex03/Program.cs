0e6bbd5f4fa486e411ed5f35a293018d0064ef2a
Vasilya <lady_drakula_94@mail.ru>
2022-10-17 22:45:55 +0500

    All mini changes added

--- Ex03/Program.cs
+++ Ex03/Program.cs
@@ -8,46 +8,70 @@
 
 Console.Clear();
 
-int[,,] CreatIt(int x, int y, int z)
+int[,,] Creat3d(int x, int y, int z)
 {
     return new int[x, y, z];
 }
 
-void FillIt(int[,,] massiv)
+int[] CreateArr(int size = 6)
 {
-    int x = massiv.GetLength(0);
-    int y = massiv.GetLength(1);
-    int z = massiv.GetLength(2);
+    return new int [size];
+}
+
+void UniqueNUmber(int [] array)
+{
+    int size = array.Length;
+    int i =0;
+    while (i <size)
+    {
+    int value = 0;
+    while(Array.IndexOf(array,value)!= -1)
+    {
+        value = Random.Shared.Next(1, 90);
+    }
+    array[i++] = value;
+}
+
+}
+
+void FillWithUniqueNumber(int[,,] array, int startValue = 10, int length = 90)
+{
+    int[] helper = CreateArr();
+    int size = helper.Length;
+    int x = array.GetLength(0);
+    int y = array.GetLength(1);
+    int z = array.GetLength(2);
     for (int i = 0; i < x; i++)
     {
         for (int j = 0; j < y; j++)
         {
             for (int k = 0; k < z; k++)
             {
-                massiv[i,j,k]=new Random().Next(1,10);
+                for(int n = 0; n <size; n++)
+                array[i,j,k] = startValue 
             }
         }
     }
 }
 
-void PrintIt(int[,,] massiv)
+void Print3d(int[,,] array)
 {
-int x = massiv.GetLength(0);
-    int y = massiv.GetLength(1);
-    int z = massiv.GetLength(2);
+int x = array.GetLength(0);
+    int y = array.GetLength(1);
+    int z = array.GetLength(2);
     for (int i = 0; i < x; i++)
     {
         for (int j = 0; j < y; j++)
         {
             for (int k = 0; k < z; k++)
             {
-                Console.Write($" {massiv[i,j,k]}({i} {j} {k}) ");
+                Console.Write($" {array[i,j,k]}({i} {j} {k}) ");
             }
             Console.WriteLine();
         }
     }
 }
 
-int[,,] horor = CreatIt(2,2,2);
-FillIt(horor);
-PrintIt(horor);
+int[,,] twodigit = Creat3d(2,2,2);
+FillWithUniqueSequences(twodigit);
+Print3d(twodigit);
