6613a8f4a159a1a6cd71731fe8d349d2921f8c2d
Vasilya <lady_drakula_94@mail.ru>
2022-10-19 21:55:58 +0500

    Ex03 не смогла сделать массив с неповторяющимися рандомнымы числами

--- Ex03/Program.cs
+++ Ex03/Program.cs
@@ -13,25 +13,36 @@ int[,,] Creat3d(int x, int y, int z)
     return new int[x, y, z];
 }
 
-int[] CreateArr(int size)
+void Fill3d(int[,,] array)
 {
-    return new int[size];
-}
-
-
-
-void Print3d(int[,,] array3d)
-{
-    int x = array3d.GetLength(0);
-    int y = array3d.GetLength(1);
-    int z = array3d.GetLength(2);
+    int x = array.GetLength(0);
+    int y = array.GetLength(1);
+    int z = array.GetLength(2);
+    int startnumber = 10;
     for (int i = 0; i < x; i++)
     {
         for (int j = 0; j < y; j++)
         {
             for (int k = 0; k < z; k++)
             {
-                Console.Write($" {array3d[i, j, k]}({i} {j} {k}) ");
+                array[i,j,k] = startnumber + 5;
+            }
+        }
+    }
+}
+
+void Print3d(int[,,] array)
+{
+    int x = array.GetLength(0);
+    int y = array.GetLength(1);
+    int z = array.GetLength(2);
+    for (int i = 0; i < x; i++)
+    {
+        for (int j = 0; j < y; j++)
+        {
+            for (int k = 0; k < z; k++)
+            {
+                Console.Write($" {array[i, j, k]}({i} {j} {k}) ");
             }
             Console.WriteLine();
         }
@@ -40,4 +51,7 @@ void Print3d(int[,,] array3d)
 
 int[,,] twodigit = Creat3d(2, 2, 2);
 
+Fill3d(twodigit);
+
 Print3d(twodigit);
+
